277e217cb273511864b6b6a90567a4ce91f60fb8
nishant <nishant.singh87@gmail.com>
2020-11-27 03:47:58 +0530

    Handle multiple files at once.

--- spike-dynamic-grpc/index.js
+++ spike-dynamic-grpc/index.js
@@ -1,17 +1,39 @@
 const path = require('path');
+const protoLoader = require('@grpc/proto-loader');
 
 const server = require('./src/server/endpoint-server');
+const analyzer = require('./src/proto/proto-analyzer');
 
 const greet = path.join(__dirname , './protos/helloworld.proto');
 
+const protoFiles = [greet];
+
+const loadFile = (protoFilePath) => {
+  return protoLoader.loadSync(
+    protoFilePath,
+    {keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true
+    });
+};
+
+const createEnpointHandler = (endpoint) => {
+  const handler = (call, send, endpoint) => {
+    const compiledMessage = {message: 'path: '+ endpoint.getId()+ ', request : ' +  call.request.name};
+    send(null, compiledMessage);
+  };
+  service.add({protoPath: greet, endpoint, onRequest: handler});
+};
+
 var service = server.create({host: "0.0.0.0", port: "50053"});
 
-service.add(greet, (call, callback) => {
-  callback(null, {message: 'I am loving it bros :  ' + call.request.name});
-  setTimeout(() => {
-    console.log("shutting down...");
-    service.stop();
-  }, 1000);
-});
+
+// Add handler for each endpoint in each proto file,
+protoFiles
+  .map(file => analyzer.readProto(loadFile(file)))
+  .reduce((arr, files) => [...arr, ...files], [])
+  .forEach(createEnpointHandler);
 
 service.start();
--- spike-dynamic-grpc/src/server/endpoint-server.js
+++ spike-dynamic-grpc/src/server/endpoint-server.js
@@ -1,14 +1,15 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
+
 module.exports = {
   create: ({host, port}) => {
-    var server = new grpc.Server();
+    const server = new grpc.Server();
     server.bind(`${host}:${port}`, grpc.ServerCredentials.createInsecure());
 
     return {
-      add : (protoPath, onRequest) => {
-        var packageDefinition = protoLoader.loadSync(
+      add : ({protoPath, endpoint, onRequest}) => {
+        const packageDefinition = protoLoader.loadSync(
           protoPath,
           {keepCase: true,
             longs: String,
@@ -16,9 +17,16 @@ module.exports = {
             defaults: true,
             oneofs: true
           });
-        let protofile = grpc.loadPackageDefinition(packageDefinition);
-        var hello_proto = protofile.helloworld;
-        server.addService(hello_proto.Greeter.service, {sayHello: onRequest});
+
+        const protofile = grpc.loadPackageDefinition(packageDefinition);
+        const hello_proto = protofile.helloworld;
+
+
+        const requestHandler = (call, callback) => {
+          onRequest(call, callback, endpoint);
+        };
+
+        server.addService(hello_proto.Greeter.service, {sayHello: requestHandler});
       },
       start: () => {
         server.start();
