9e60954a0bab78d86557f31c0491c3ebe8817276
nishant <nishant.singh87@gmail.com>
2020-11-27 12:56:41 +0530

    refactor server test.

--- spike-dynamic-grpc/tests/server.test.js
+++ spike-dynamic-grpc/tests/server.test.js
@@ -1,4 +1,4 @@
-const {helloProto, helloProtoFilePath} = require('./test-helper');
+const {helloProto} = require('./test-helper');
 
 const server = require('../src/server/endpoint-server');
 const analyzer = require('../src/proto/proto-analyzer');
@@ -11,9 +11,17 @@ const url = `${host}:${port}`;
 
 describe('server.test.js', () => {
   let service ;
+  const responses = {};
+  const  endpoint = analyzer.readProto(helloProto).pop();
 
   beforeEach(() => {
     service = server.create({host, port});
+    const handler = (call, send, endpoint) => {
+      send(null, responses[endpoint.getId()]);
+    };
+
+    service.add({protoFile: helloProto, endpoint , onRequest: handler});
+    service.start();
   });
 
   afterEach(() => {
@@ -21,22 +29,13 @@ describe('server.test.js', () => {
   });
 
   test('should handle a unary request', async () => {
+    const expectedResponseMessage = 'hello world message';
 
-    const handler = (call, send, endpoint) => {
-      const message = `${endpoint.getId()} : hello ${call.request.name}`;
-      send(null, {message});
-    };
-
-    const  endpoint = analyzer.readProto(helloProto).pop();
-
-    service.add({protoFile: helloProto, endpoint , onRequest: handler});
-    service.start();
+    // Setup response
+    responses[endpoint.getId()] = {message: expectedResponseMessage};
 
     const hello_proto = grpc.loadPackageDefinition(helloProto).helloworld.greet;
     const client = new hello_proto.Greeter(url, grpc.credentials.createInsecure());
-
-    const expectedResponseMessage = 'helloworld.greet.Greeter/SayHello : hello nishant';
-
     await new Promise((resolve) => {
       client.sayHello({name: "nishant"}, function(err, response) {
         expect(response.message).toBe(expectedResponseMessage)
