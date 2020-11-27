ef05fe218e00faca2f19765785ea4d9720b213ae
nishant <nishant.singh87@gmail.com>
2020-11-28 04:15:10 +0530

    update index.js to run greeter prot.

--- spike-dynamic-grpc/index.js
+++ spike-dynamic-grpc/index.js
@@ -4,9 +4,10 @@ const protoLoader = require('@grpc/proto-loader');
 const server = require('./src/server/endpoint-server');
 const analyzer = require('./src/proto/proto-analyzer');
 
-const pricesProtoFile = path.join(__dirname , './protos/prices.proto');
+const pricesProtoFile = path.join(__dirname , './tests/fixtures/protos/prices.proto');
+const helloWorldProtoFile = path.join(__dirname , './tests/fixtures/protos/helloworld.proto');
 
-const protoFiles = [pricesProtoFile];
+const protoFiles = [pricesProtoFile, helloWorldProtoFile];
 
 const loadFile = (protoFilePath) => {
   return protoLoader.loadSync(
@@ -19,12 +20,23 @@ const loadFile = (protoFilePath) => {
     });
 };
 
-const createEnpointHandler = (endpoint) => {
-  const handler = (call, send, endpoint) => {
+const handlers = {
+  'helloworld.greet.Greeter.SayHello' : (call, send) => {
+    send(null, {message : `hello ${call.request.name}`});
+  },
+  'prices.streaming.Pricing.Subscribe' : (call) => {
     let i = 0;
-    setInterval(() => call.write({quote : `quote${i++}`}), 1000)
+    setInterval(() => call.write({quote : `quote${i++}`}), 1000);
+  }
+};
+
+const createEnpointHandler = ({endpoint, loadedProtofile}) => {
+  const handler = (call, send, endpoint) => {
+    let id = endpoint.getId();
+    const handler = handlers[id];
+    handler(call, send);
   };
-  service.add({protoFile: loadFile(pricesProtoFile), endpoint, onRequest: handler});
+  service.add({protoFile: loadedProtofile, endpoint, onRequest: handler});
 };
 
 var service = server.create({host: "0.0.0.0", port: "50053"});
@@ -32,7 +44,13 @@ var service = server.create({host: "0.0.0.0", port: "50053"});
 
 // Add handler for each endpoint in each proto file,
 protoFiles
-  .map(file => analyzer.readProto(loadFile(file)))
+  .map(file => {
+    const loadedProtofile = loadFile(file);
+    return analyzer.readProto(loadedProtofile).map(endpoint => ({
+      endpoint,
+      loadedProtofile
+    }));
+  })
   .reduce((arr, files) => [...arr, ...files], [])
   .forEach(createEnpointHandler);
 
