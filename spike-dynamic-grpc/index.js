d539d8ba7fb8fc4534f27463c50bbc5129b915b1
nishant <nishant.singh87@gmail.com>
2020-11-28 18:17:36 +0530

    Refactor index.js : create handlers for reqeust type and resonse compiler.

--- spike-dynamic-grpc/index.js
+++ spike-dynamic-grpc/index.js
@@ -20,31 +20,56 @@ const loadFile = (protoFilePath) => {
     });
 };
 
-const handlers = {
-  'helloworld.greet.Greeter.SayHello' : (call, send) => {
-    send(null, {message : `hello ${call.request.name}`});
-  },
-  'prices.streaming.Pricing.Subscribe' : async (call) => {
-    const responses = [{quote: "quote:one"}, {quote: "quote:two"}, {quote: "quote:three"}, {end: true}];
-    for(let i =0; i < responses.length; i++){
-      await new Promise((resolve => setTimeout(resolve, 500)));
-      const response = responses[i];
-      if(response.end) {
-        call.end();
-      }
-      else {
-        call.write(response);
-      }
+const getHandler = (endpoint) => {
+  const type = endpoint.getResponse().isStream() ? "response-stream" : "unary";
+
+  const handlerTypes = {
+    "unary" : (response) => {
+      return (call, send) => {
+        send(null, response);
+      };
+    },
+    "response-stream" : (response) => {
+      return async (call) => {
+        const responses = [...response.stream, {end: true}];
+        for(let i =0; i < responses.length; i++){
+          await new Promise((resolve => setTimeout(resolve, 500)));
+          const response = responses[i];
+          if(response.end) {
+            call.end();
+          }
+          else {
+            call.write(response);
+          }
+        }
+      };
     }
-  }
+  };
+
+  return handlerTypes[type];
+}
+
+const endpointResponseReader = (endpointId) => {
+  const mappings = {
+    'helloworld.greet.Greeter.SayHello' : (call) => {
+      return {message : `hello ${call.request.name}`};
+    },
+    'prices.streaming.Pricing.Subscribe' : (call) => {
+      return {stream: [{quote: "quote:one"}, {quote: "quote:two"}, {quote: "quote:three"}]};
+    }
+  };
+  return mappings[endpointId];
 };
 
 const createEnpointHandler = ({endpoint, loadedProtofile}) => {
-  const handler = (call, send, endpoint) => {
-    let id = endpoint.getId();
-    const handler = handlers[id];
-    handler(call, send);
+  const handler = async (call, send, endpoint) => {
+    const responseCompiler = endpointResponseReader(endpoint.getId());
+    const responseSender = getHandler(endpoint);
+
+    const response = responseCompiler(call);
+    await responseSender(response)(call, send);
   };
+
   service.add({protoFile: loadedProtofile, endpoint, onRequest: handler});
 };
 
