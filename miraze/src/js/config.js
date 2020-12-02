5618c805e74e18eb1ee97a822463144f1a7f0544
nishant <nishant.singh87@gmail.com>
2020-12-02 11:17:10 +0530

    Resolve spike.

--- miraze/src/js/config.js
+++
@@ -1,9 +0,0 @@
-const DEFAULT_KEYWORD_SUFFIX = '@';
-
-const config = {
-  isKeyWord : (str, keyword) => {
-    const regex = `^${keyword}${DEFAULT_KEYWORD_SUFFIX}$`;
-    return new RegExp(regex).test(str);
-  }
-};
-module.exports = config;
