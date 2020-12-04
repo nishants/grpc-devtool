9cbc0b4cb4064614b6e1695c3bdf2236af331aab
nishant <nishant.singh87@gmail.com>
2020-12-04 14:28:29 +0530

    handle array in matcher tree.

--- src/mapping/matcher-tree/matcher-tree.js
+++ src/mapping/matcher-tree/matcher-tree.js
@@ -5,6 +5,10 @@ const isObject = (object) =>
 
 const MatcherTree = {
   create: (root) => {
+    if(Array.isArray(root)){
+      return root.map(node => isObject(node) ? MatcherTree.create(node) : MatcherNode.create(node));
+    }
+
     const tree = {};
     for(const fieldName in root){
       const fieldValue = root[fieldName];
--- tests/mapping/matcher-tree.test.js
+++ tests/mapping/matcher-tree.test.js
@@ -1,6 +1,12 @@
 const MatchingTree = require('../../src/mapping/matcher-tree/matcher-tree');
 
 const getSpecsFromTree = (tree => {
+  if(Array.isArray(tree)){
+    return tree.map(value => {
+      return value.isNode ? value.spec : getSpecsFromTree(value);
+    });
+  }
+
   const valueTree = {};
   for(const fieldName in tree){
     const fieldValue = tree[fieldName];
@@ -11,16 +17,17 @@ const getSpecsFromTree = (tree => {
 
 describe("matcher-tree.js", () => {
 
-  test("should generate a flat tree", () => {
+  test("should generate a  tree", () => {
     const spec = {
       value : 21,
-      // array : [1, 2, 3, 4],
+      array : [1, 2, 3, {value: "nestedObjeInArray", nestedArray: [{value: "deeplyNestedObjecInArray"}]}],
       object: {
         objectValue: "object value",
         objectValueOther: "object value other",
         nestedObject: {
           nestedObjectValue: "nested object value",
-          nestedObjectValueOther: "nested object value other"
+          nestedObjectValueOther: "nested object value other",
+          deeplyNestedArray : [1, 2, 3, {value: "nestedObjeInArray", nestedArray: [{value: "deeplyNestedObjecInArray"}]}]
         }
       }
     };
