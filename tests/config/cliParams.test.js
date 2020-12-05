3392c35cef0129810eb5e5e284565e7208b63ba0
nishant <nishant.singh87@gmail.com>
2020-12-06 05:27:36 +0530

    Conver cli input to params

--- tests/config/cliParams.test.js
+++ tests/config/cliParams.test.js
@@ -2,20 +2,62 @@ const cliParams = require('../../src/config/cliParams');
 
 describe('getConfigPath.js', () => {
 
-  const pathToFixtures = cliParams.parse('');
-
   test('should get output dir from user', () => {
-    const cliInput = "";
+    const cliInput = [
+      'path/to/config with space',
+      'port=3202',
+      'protos="path/to/protos with space"',
+      'remote=remote.host:121',
+      'streamingLoopSize=1212',
+      'host=localhost'
+    ]
+
     const expected = {
         host: 'localhost',
         port: 3202,
         configPath : 'path/to/config with space',
-        protosPath: 'path/to/protos',
+        protosPath: 'path/to/protos with space',
         remoteHost: 'remote.host',
-        remotePort: 'remote.port',
+        remotePort: 121,
         streamingLoopSize: 1212
-      }
-    ;
+      };
+
+    const actual = cliParams.parse(cliInput)
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should accept empty array', () => {
+    const cliInput = []
+
+    const expected = {};
+
+    const actual = cliParams.parse(cliInput)
+
+    expect(actual).toEqual(expected);
+  });
+
+
+  test('should accept only config path', () => {
+    const cliInput = ['"path/to/config with space"']
+
+    const expected = {
+      configPath: "path/to/config with space"
+    };
+
+    const actual = cliParams.parse(cliInput)
+
+    expect(actual).toEqual(expected);
+  });
+
+
+  test('should accept path withotu quotes', () => {
+    const cliInput = ['path/to/config']
+
+    const expected = {
+      configPath: "path/to/config"
+    };
+
     const actual = cliParams.parse(cliInput)
 
     expect(actual).toEqual(expected);
