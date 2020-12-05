8b6aaa267e56a907c59bcdfdb64fb33cd16bda55
nishant <nishant.singh87@gmail.com>
2020-12-05 23:10:13 +0530

    Added SelectProtosToMap.js

---
+++ src/init/interactive/SelectProtosToMap.js
@@ -0,0 +1,35 @@
+
+
+module.exports = {
+  create : (prevConfig) => {
+    let config = {
+      ...prevConfig,
+      protosToMap: []
+    };
+
+    const protoFiles = config.protoFiles;
+    let nextFile = 0;
+
+    return {
+      needsMoreInput: () => {
+        return nextFile !== protoFiles.length;
+      },
+      getNextInputQuestion: () => {
+        const file = protoFiles[nextFile];
+        return `Generate default mappings for ${file} (y/n) ? (y)`;
+      },
+      addInput: (input) => {
+        const ignore = ['n', 'no'].includes(input.trim().toLowerCase());
+
+        if(ignore){
+          return nextFile++;
+        }
+
+        config.protosToMap.push(protoFiles[nextFile++]);
+      },
+      getConfig: () => {
+        return config;
+      }
+    };
+  }
+};
