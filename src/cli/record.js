0be7a80f8190fb2909c7e4da7edd4070115ac848
nishant <nishant.singh87@gmail.com>
2020-12-06 06:12:08 +0530

    Serve with dynamic params.

--- src/cli/record.js
+++ src/cli/record.js
@@ -1,7 +1,6 @@
 const app = require('../app');
 const config = require('../config/index');
 
-
 module.exports = {
   run : async (commandLineParams) => {
     const appConfig = await config.getAppConfig(commandLineParams);
