fd1c44e0419cb1f3eb36be7e82c2675fbaf6e967
nishant <nishant.singh87@gmail.com>
2020-12-06 18:13:12 +0530

    Fix memory leak and hanging on help text.

--- src/init/index.js
+++ src/init/index.js
@@ -2,42 +2,46 @@ const readLine = require('readline');
 const chalk = require('chalk');
 const init = require('./init');
 
-const states = [
-  require('./interactive/SelectProtosToMap'),
-  require('./interactive/CreateDefaultMappings'),
-  require('./interactive/GetProtosPath'),
-  require('./interactive/GetHostConfig'),
-  require('./interactive/GetOutputDir'),
-];
-
-const cli = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-const askUser = (prompt) => {
-  return new Promise((resolve) => {
-    if(prompt.error){
-      console.log(chalk.red(prompt.error));
-    }
-    if(prompt.question){
-      console.log(chalk.green(prompt.question));
-    }
-    if(prompt.default){
-      console.log(chalk.dim(prompt.default));
-    }
-
-    cli.on('line', (userInput) => {
-      resolve(userInput);
-    });
-  });
-};
-
 module.exports = {
   createProject : async (initialConfig) => {
+    const states = [
+      require('./interactive/SelectProtosToMap'),
+      require('./interactive/CreateDefaultMappings'),
+      require('./interactive/GetProtosPath'),
+      require('./interactive/GetHostConfig'),
+      require('./interactive/GetOutputDir'),
+    ];
+
+    const cli = readLine.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    let nextResolve = null;
+
+    const askUser = (prompt) => {
+      return new Promise((resolve) => {
+        nextResolve = resolve;
+        if(prompt.error){
+          console.log(chalk.red(prompt.error));
+        }
+        if(prompt.question){
+          console.log(chalk.green(prompt.question));
+        }
+        if(prompt.default){
+          console.log(chalk.dim(prompt.default));
+        }
+      });
+    };
+
+    cli.on('line', (userInput) => {
+      if(nextResolve) nextResolve(userInput);
+    });
+
+    process.setMaxListeners(0);
     let config = {...initialConfig};
     while(states.length){
-      const next = states.pop().create(config);
+      const next = await states.pop().create(config);
       while(next.needsMoreInput()){
         const input = await askUser(next.getNextInputQuestion());
         await next.addInput(input);
