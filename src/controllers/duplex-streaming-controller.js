0876603cae5ca71e22ccdac70aa608977f66e23c
nishant <nishant.singh87@gmail.com>
2021-02-14 18:58:54 +0530

    Test for matching array of templates for client straming.

--- src/controllers/duplex-streaming-controller.js
+++ src/controllers/duplex-streaming-controller.js
@@ -1,22 +1,5 @@
 const DEFAULT_STREAMING_DELAY = 1000;
 
-const getRequest = (callContext) => {
-  return new Promise((resolve, reject) => {
-    const data = [];
-    // TODO Currenlty resolves on  first data from client
-    callContext.on('data', (response) => {
-      data.push(response)
-      resolve(response);
-    });
-    callContext.on('end', () => {
-      if(data.length === 0){
-        reject(`Empty message received from client at ${endpointId}`);
-      }
-      resolve(null);
-    });
-  });
-};
-
 module.exports = {
   create: async ({endpoint, mappingResolver, dataFiles}) => {
     return {
@@ -25,25 +8,33 @@ module.exports = {
         return endpoint.getId() === endpointId;
       },
       handle : async (callContext) => {
-        const request = await getRequest(callContext);
-        const responseFile = mappingResolver.getResponseFile(endpoint.getId(), request);
-        const template = await dataFiles.get(responseFile);
-        const response = template.getResponse().compile();
+        callContext.on('end', () => {
+          callContext.end();
+        });
 
-        if(!response){
-          return callContext.end();
-        }
+        const respondToClientMessage = async (request) => {
+          console.log(`Received request for ${endpoint.getId()} `, request);
+          const responseFile = mappingResolver.getResponseFile(endpoint.getId(), request);
+          const template = await dataFiles.get(responseFile);
+          const response = template.getResponse().compile();
 
-        const responses = [...response.stream];
-        const streamingDelay = typeof response.streamInterval === 'undefined' ? DEFAULT_STREAMING_DELAY : response.streamInterval;
-        let keepStreaming = !response.doNotRepeat ;
+          if(!response){
+            return callContext.end();
+          }
 
-        for(let i =0; i < responses.length || keepStreaming; i++){
-          await new Promise((resolve => setTimeout(resolve, streamingDelay)));
-          callContext.write(responses[i%responses.length]);
-        }
+          const responses = [...response.stream];
+          const streamingDelay = typeof response.streamInterval === 'undefined' ? DEFAULT_STREAMING_DELAY : response.streamInterval;
+          let keepStreaming = !response.doNotRepeat ;
 
-        callContext.end();
+          for(let i =0; i < responses.length || keepStreaming; i++){
+            await new Promise((resolve => setTimeout(resolve, streamingDelay)));
+            callContext.write(responses[i%responses.length]);
+          }
+        };
+
+        callContext.on('data', (response) => {
+          respondToClientMessage(response);
+        });
       }
     }
   }
