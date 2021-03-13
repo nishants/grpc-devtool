635a66d45c383d361ed4c5cbade9fab3919e0adb
nishant <nishant.singh87@gmail.com>
2021-03-13 16:27:15 +0530

    Move css and html in assets.

--- app/src/index.js
+++ app/src/index.js
@@ -14,7 +14,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname,"..", "assets", 'index.html'));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
