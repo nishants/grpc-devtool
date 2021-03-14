832338aa5bcfccf7a5324b3a9dc6ba51a022a8ff
nishant <nishant.singh87@gmail.com>
2021-03-14 19:26:15 +0530

    Add router.

--- ui/ui/app.js
+++ ui/ui/app.js
@@ -1,13 +1,114 @@
 import React from "react";
-import WelcomeScreen from "./modules/welcome-screen";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const App = () => {
-  return <>
-    <h1>Grpc Devtool UI</h1>
-    <p>The react project.</p>
-    <WelcomeScreen/>
-  </>;
+
+const routes = [
+  {
+    path: "/welcome",
+    component: Welcome
+  },
+  {
+    path: "/projects",
+    component: Projects,
+    routes: [
+      {
+        path: "/projects/create",
+        component: CreateProject
+      },
+      {
+        path: "/projects/open",
+        component: OpenProject
+      }
+    ]
+  }
+];
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// A special wrapper for <Route> that knows how to
+// handle "sub"-routes by passing them in a `routes`
+// prop to the component it renders.
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
+
+function Welcome() {
+  return (
+    <div>
+      <h2>Tacos</h2>
+      <ul>
+        <li>
+          <Link to="/projects/create">Create</Link>
+        </li>
+        <li>
+          <Link to="/projects/open">Open</Link>
+        </li>
+      </ul>
+
+    </div>
+  );
 };
 
-export default App;
+function Projects({ routes }) {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <Link to="/projects/create">Create</Link>
+        </li>
+        <li>
+          <Link to="/projects/open">Open</Link>
+        </li>
+      </ul>
 
+      <Switch>
+        {routes.map((route, i) => (
+          <RouteWithSubRoutes key={i} {...route} />
+        ))}
+      </Switch>
+    </div>
+  );
+}
+
+function CreateProject() {
+  return <h3>Create configuration for project</h3>;
+}
+
+function OpenProject() {
+  return <h3>Select dir containing project</h3>;
+}
--- ui/ui/modules/welcome-screen/index.js
+++ ui/ui/modules/welcome-screen/index.js
@@ -9,48 +9,22 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
-const onFinish = (values) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
-const WelcomeScreenContainer = () => {
+const WelcomeScreenContainer = ({openProject, createNewProject}) => {
   return <div className="welcome-screen">
     <Form
       {...layout}
       name="basic"
       initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
+    ><Form.Item {...tailLayout}>
+      <Button type="primary" htmlType="submit" onClick={openProject}>
+        Open Project
+      </Button>
       <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
+        <Button type="secondary" htmlType="submit" onClick={createNewProject}>
+          Create Project
         </Button>
       </Form.Item>
+    </Form.Item>
     </Form>
   </div>;
 };
