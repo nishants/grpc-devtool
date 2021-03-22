1b1bf4f1e78e15f6c711994841ca20f5639c4544
nishant <nishant.singh87@gmail.com>
2021-03-22 16:58:08 +0530

    Add redux saga to ui.

--- ui/ui/store.js
+++ ui/ui/store.js
@@ -1,4 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+const sagaMiddleware = createSagaMiddleware();
+
+import "regenerator-runtime/runtime";
 import rootReducer from './rootReducer';
-const store = createStore(rootReducer);
+import rootSaga from './rootSaga';
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+
 export default store;
